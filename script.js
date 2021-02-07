461d33c2c4d01e6334da2b420038390795ec16b0
alexdermenji <allexdermenji@gmail.com>
2021-02-07 06:58:32 +0000

    geolocation service

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  position => {
+    console.log('good');
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+    console.log(link);
+  },
+  function () {
+    alert('Oops somethong wrong');
+  }
+);
