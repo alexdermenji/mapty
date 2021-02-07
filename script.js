b45c4271511310cd99015e355c28cb0fc0c2cd67
alexdermenji <allexdermenji@gmail.com>
2021-02-07 10:08:25 +0000

    marker and popup on the map

--- script.js
+++ script.js
@@ -11,16 +11,42 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) console.log('hey');
-navigator.geolocation.getCurrentPosition(
-  position => {
-    console.log('good');
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-    console.log(link);
-  },
-  function () {
-    alert('Oops somethong wrong');
-  }
-);
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const link = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+      console.log(link);
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 16);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Oops somethong wrong');
+    }
+  );
+}
